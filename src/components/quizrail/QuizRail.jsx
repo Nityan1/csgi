92917927d64de8d771dcd5bd2a5d0158b1840dc1
Krishanu <krishanuprabhu@gmail.com>
2023-04-05 21:55:00 +0530

    Quiz updates 1st commit

--- src/components/quizrail/QuizRail.jsx
+++ src/components/quizrail/QuizRail.jsx
@@ -1,20 +1,33 @@
+import { useEffect, useState } from "react";
 import "./QuizRail.css"
 
-export default function QuizRail (data = []){
+
+
+export default function QuizRail(data) {
+    // console.log('asdas', data);
+    const [selectedQuiz, setSelectedQuiz] = useState(data.data[0])
+
+    useEffect(() => {
+        console.log('Selected QUiz is /', selectedQuiz)
+    }, [selectedQuiz])
+
+    function QuizRailCard({ labelData, active }) {
+        return (
+            <div onClick={() => { setSelectedQuiz(labelData) }} className={`quizCard ${active && 'cardActive'}`}>
+                {labelData.company_name}
+            </div>
+        )
+    }
+
+    
     return (
         <div className="quizRailContainer">
-            <QuizRailCard label="Quiz 1" active/>
-            <QuizRailCard label="Quiz 2" />
-            <QuizRailCard label="Quiz 3" />
-            <QuizRailCard label="Quiz 4" />
+
+            {
+                data.data.map(listItem => { return (<QuizRailCard labelData={listItem} active={selectedQuiz.company_name === listItem.company_name ? true : false} />) })
+            }
+
         </div>
     )
 }
 
-function QuizRailCard({label, active}) {
-    return (
-        <div className={`quizCard ${active && 'cardActive'}`}>
-            {label}
-        </div>
-    )
-}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,36 +1,40 @@
+import { useNavigate } from "react-router-dom"
 import "./Sidebar.css"
 
 import React from "react"
 
 function Sidebar() {
+    // const navigate = useNavigate();
     return (
         <div className="sidebar">
             <SidebarTitle />
             <div className="sidebarContainer">
-                <SidebarCard iconPath="/img/dashboard-icon.png" label="Dashboard"/>
-                <SidebarCard iconPath="/img/quiz-icon.png" label="Quiz" active/>
-                <SidebarCard iconPath="/img/profile-icon.png" label="Profile" />
-                <SidebarCard iconPath="/img/analysis-icon.png" label="Analysis" />
+                <SidebarCard iconPath="/img/dashboard.png" label="Dashboard"  route="Dashboard" />
+                <SidebarCard iconPath="/img/edit.png" label="Quiz" active route="Quiz" />
+                <SidebarCard iconPath="/img/profile.png" label="Profile" route="Profile" />
+                <SidebarCard iconPath="/img/analysis.png" label="Analysis" route="Analysis" />
             </div>
-            
+
         </div>
     )
 }
 
 function SidebarTitle() {
     return (
-         <div className="sidebarTitle">
+        <div className="sidebarTitle">
             <img src="/img/logo.png" alt="sidebarlogo" className="titleImg" />
             <div className="titleText">
                 CSGI DISCORSO
             </div>
-         </div>
+        </div>
     )
 }
 
-function SidebarCard({iconPath, label, active}) {
+function SidebarCard({ iconPath, label, active, route }) {
+    const navigate = useNavigate();
+
     return (
-        <div className={`sidebarTab ${active && 'active'}`}>
+        <div onClick={() => { navigate(route) }} className={`sidebarTab ${active && 'active'}`}>
             <img src={iconPath} alt={label} className="sidebarImg" />
             <div>{label}</div>
         </div>
