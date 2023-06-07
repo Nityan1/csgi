196b2edf466eaebf6240ad5cb3aee5daa009a4b6
Krishanu <krishanuprabhu@gmail.com>
2023-06-08 02:55:56 +0530

    Left out final

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,12 +1,18 @@
-function Profile() {
-    return (
-        <>
-            <div>
+import React from "react"
+import "./Setting.css"
 
-                <p>Coming Soon</p>
-            </div>
-        </>
+function Setting () {
+
+    return(
+        <div className="mainDiv">
+        <h1 className="heading">SETTING</h1>
+       <div className="btnContainer">
+        <button className="logbtn">Logout</button>
+        <button className="cpbtn">Change Password</button>
+       </div>
+
+    </div>
     )
 }
 
-export default Profile
+export default Setting
